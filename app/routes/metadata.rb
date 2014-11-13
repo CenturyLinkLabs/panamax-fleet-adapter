387bb39dd58cd547e2c8e20410de0b67c18af871
Don Petersen <don@donpetersen.net>
2014-11-13 17:21:14 +0000

    Use namespace Sinatra helper.

--- app/routes/metadata.rb
+++ app/routes/metadata.rb
@@ -1,13 +1,15 @@
 module FleetAdapter
   module Routes
     class Metadata < Base
-      get "/#{API_VERSION}/metadata" do
-        json(
-          {
-            version: FleetAdapter::VERSION,
-            type: "Fleet"
-          }
-        )
+      namespace '/' + API_VERSION do
+        get '/metadata' do
+          json(
+            {
+              version: FleetAdapter::VERSION,
+              type: "Fleet"
+            }
+          )
+        end
       end
     end
   end
--- app/routes/services.rb
+++ app/routes/services.rb
@@ -6,43 +6,45 @@ module FleetAdapter
   module Routes
     class Services < Base
 
-      post "/#{API_VERSION}/services" do
-        sorted_services = ServiceSorter.sort(@payload)
-        services = Service.create_all(sorted_services)
-        services.each(&:start)
-        status 201
-        json services.map { |service| { id: service.id } }
-      end
-
-      get "/#{API_VERSION}/services/:id" do
-        service = Service.find(params[:id])
-
-        result = {
-          id: service.id,
-          'actualState' => service.status
-        }
-
-        json result
-      end
-
-      put "/#{API_VERSION}/services/:id" do
-        service = Service.find(params[:id])
-
-        case @payload[:desiredState]
-        when 'started'
-          service.start
-          status 204
-        when 'stopped'
-          service.stop
-          status 204
-        else
-          status 400
+      namespace '/' + API_VERSION do
+        post '/services' do
+          sorted_services = ServiceSorter.sort(@payload)
+          services = Service.create_all(sorted_services)
+          services.each(&:start)
+          status 201
+          json services.map { |service| { id: service.id } }
         end
-      end
 
-      delete "/#{API_VERSION}/services/:id" do
-        Service.find(params[:id]).destroy
-        status 204
+        get '/services/:id' do
+          service = Service.find(params[:id])
+
+          result = {
+            id: service.id,
+            'actualState' => service.status
+          }
+
+          json result
+        end
+
+        put '/services/:id' do
+          service = Service.find(params[:id])
+
+          case @payload[:desiredState]
+          when 'started'
+            service.start
+            status 204
+          when 'stopped'
+            service.stop
+            status 204
+          else
+            status 400
+          end
+        end
+
+        delete '/services/:id' do
+          Service.find(params[:id]).destroy
+          status 204
+        end
       end
     end
   end
